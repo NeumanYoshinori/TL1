4b7e78415389e3bca19740d0792b6c53e87d719f
YoshinoriNeuman <k024g0084@g.neec.ac.jp>
2026-06-08 12:13:48 +0900

    level_editor.pyをファイルに追加

---
+++ level_editor.py
@@ -0,0 +1,87 @@
+import bpy
+
+# ブレンダーに登録するアドオン情報
+bl_info = {
+    "name": "レベルエディタ",
+    "author": "Taro Kamata",
+    "version": (1, 0),
+    "blender": (3, 3, 1),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object"
+}
+
+# メニュー項目描画
+def draw_menu_manual(self, context):
+    #self : 呼び出し元の暮らすインスタンス。C++でいうthisポインタ
+    #context : カーソルを合わせたときのポップアップのカスタマイズなどに使用
+
+    #トップバーの「エディターメニュー」に項目（オペレータ）を追加
+    self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+#オペレータ　頂点を伸ばす
+class MYADDON_OT_stretch_vertex(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_stretch_vertex"
+    bl_label = "頂点を伸ばす"
+    bl_description = "頂点座標を引っ張って伸ばします"
+    #リドゥ、アンドゥ可能オプション
+    bl_options = {'REGISTER', 'UNDO'}
+
+    #メニューを実行したときに呼ばれるコールバック関数
+    def execute(self, context):
+        bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+        print("頂点を伸ばしました。")
+
+        #オペレータの命令終了を通知
+        return {'FINISHED'}
+
+#トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    #Blenderがクラスを識別する為の固有の文字列
+    bl_idname = "TOPBAR_MT_my_menu"
+    #メニューのラベルとして表示される文字列
+    bl_label = "MyMenu"
+    #著者表示用の文字列
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    # サブメニューの描画
+    def draw(self, context):
+
+        #トップバーの「エディタメニュー」に項目（オペレータ）を追加
+        self.layout.operator(MYADDON_OT_stretch_vertex.bl_idname,
+            text=MYADDON_OT_stretch_vertex.bl_label)
+        
+    # 既存のメニューにサブメニューを追加
+    def submenu(self, context):
+
+        #ID指定でサブメニューを追加
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+# Blenderに登録するクラスリスト
+classes = (
+    MYADDON_OT_stretch_vertex,
+    TOPBAR_MT_my_menu,
+)
+
+#Add-On有効化時コールバック
+def register():
+    # Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    #メニューに項目を追加
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
+    print("レベルエディタが有効化されました。")
+
+#Add-On無効化時コールバック
+def unregister():
+    #メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(draw_menu_manual)
+    print("レベルエディタが無効化されました。")
+
+#テスト実行用コード
+if __name__ == "__main__":
+    register()
